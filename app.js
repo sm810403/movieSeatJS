f5b9cac4854ebda6d82ef5f4bd54192e94d5bf8d
JaeIn <sm810403@hotmail.com>
2020-09-15 22:36:08 -0700

    second commit //small adj

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const seats = document.querySelectorAll('.seat:not(.o-seat)');
-const tickets = document.querySelector('.tickets')
+const tickets = document.querySelector('.tickets');
+const total = document.querySelector('.total');
+const movieSelect = document.getElementById('movie-select')
 
-
+const ticketPrice = +movieSelect.value;
+console.log(typeof ticketPrice)
 seats.forEach((seat)=>{
     seat.addEventListener('click',(e)=>{
-        if (!e.target.classList.contains('s-seat')){
-            e.target.classList.add('s-seat')
-        } else {
-            e.target.classList.remove('s-seat')
-        }
-        updateTotal();
+        if (e.target.classList.contains('seat')&&!e.target.classList.contains('o-seat')){
+            e.target.classList.toggle('s-seat');
+            updateUI();
+        }  
+        
     }) 
 })
-function updateTotal(){
-    
+let totalSeats = [];
+function updateUI(){
+    const selectedSeat = document.querySelectorAll('.row.seat.s-seat');
+    const selectedSeatCount = totalSeats.length;
+    console.log(selectedSeatCount)
+    tickets.innerText = selectedSeatCount;
+    total.innerText = selectedSeatCount*ticketPrice;
 }
+// movieSelect.addEventListener('change',function(){
+
+// })
